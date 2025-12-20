c685d617a4dc1aa83e3b0ef586f3274cc821c145
IvyYang <ivy990030@icloud.com>
2025-12-20 16:05:58 +0800

    update: adjust title color

--- src/components/SuccessCasesSection.jsx
+++ src/components/SuccessCasesSection.jsx
@@ -9,7 +9,7 @@ const SuccessCasesSection = () => {
   return (
     <>
       <section className="w-full lg:max-w-360 lg:mx-auto">
-        <h2 className="heading-2! text-center border border-Primary-Blue-300 mb-4">
+        <h2 className="heading-2! text-center border border-Primary-Blue-300 mb-4 text-Neutral-700">
           成功案例
         </h2>
         <div className="grid grid-rows-2 grid-flow-col gap-7 overflow-x-auto snap-x snap-mandatory ml-12 pr-6">
