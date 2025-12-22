e0e2e1bd1ff33d7d641d557faa2de1a1f808a9a7
tsfmakko <racingf1@gmail.com>
2025-12-22 18:29:42 +0800

    feat: PartnersSection component init

---
+++ src/components/PartnersSection.jsx
@@ -0,0 +1,10 @@
+import { div, section } from "motion/react-client";
+
+const PartnersSection = () => {
+
+    return (
+        <section>PartnersSection</section>
+    )
+};
+
+export default PartnersSection;
