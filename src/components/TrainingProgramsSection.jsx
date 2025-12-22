aa37457c7dd5d38d51a77c5df202689a7d7c2fad
IvyYang <ivy990030@icloud.com>
2025-12-23 00:23:43 +0800

    update: refactor cards by map function

--- src/components/TrainingProgramsSection.jsx
+++ src/components/TrainingProgramsSection.jsx
@@ -6,12 +6,80 @@ import coachWeijie from "../assets/imgs/coach/coach-weijie-3x.png";
 import coachYinmin from "../assets/imgs/coach/coach-yinmin-3x.png";
 import coachJustin from "../assets/imgs/coach/coach-justin-3x.png";
 import coachCasper from "../assets/imgs/coach/coach-casper-3x.png";
+import { useState } from "react";
+
+const coachInfo = [
+  {
+    id: 1,
+    name: "廖洧杰",
+    title: "JS 組教練",
+    experience: [
+      "2016-2022 過往經歷：六角學院校長、高雄火箭隊前端教練",
+      "2013-2019 成功案例：協助無資訊背景轉職工程師人數超過 500 位",
+      "2013-2019 授課人數：線上+線下授課學員超過 25,000 位",
+      "2014-2019 線下授課：高雄大學前端領域兼任講師",
+      "2007-2019 實務經驗：經手超過 100 個實際專案，其領域不乏中小企業、政府專案、銀行系統",
+    ],
+  },
+  {
+    id: 2,
+    name: "穎旻",
+    title: "JS 組教練",
+    experience: [
+      "六角學院前端工程師",
+      "六角學院前端講師",
+      "金龍國小教育訓練講師",
+      "國泰產險教育訓練講師",
+    ],
+  },
+  {
+    id: 3,
+    name: "賈斯汀",
+    title: "後端教練",
+    experience: [
+      "高雄市警察局縣市合併內外部整合",
+      "嘉義縣警察局全球資訊網與內部知識網",
+      "總統府全球資訊網系統維護及開發、總統府 APP 新聞即時通系統開發",
+      "金管會 EIP SSO 整合",
+      "經濟部工業局電子書包加值應用計畫系統規劃、設計開發",
+      "國立海生館活動與報名系統、海洋教育網規劃開發",
+      "台灣淨水器材百科商務平台規劃開發、威錦水器材百科補助計畫",
+      "高雄市小學電子書包試行計畫",
+      "supermediastore（美國購物網站）系統開發",
+      "高雄捷運網站、高雄世運志工服務系統、高雄市 85 大樓招商網",
+      "高雄市國稅局全球資訊網及稅務宣導活動網站",
+      "高雄市民政局業務資訊化委外作業、新聞處圖文影像管理系統",
+      "高雄市政府文化局、衛生局中英文全球資訊網",
+      "屏東縣政府旅遊網、招商網",
+      "行政院南區服務中心網站",
+      "經濟部水利署南區水資源局 105 年度業務及全球資訊網站維護及功能擴充計畫",
+    ],
+  },
+  {
+    id: 4,
+    name: "卡斯伯",
+    title: "UI 教練",
+    experience: [
+      "2016-2022 過往經歷：六角學院共同創辦人",
+      "2016-2022 近期授課經驗：Vue 3 直播班講師、從 Figma 到 VSCode，設計做到網頁切版",
+      "2013-2016 實務經驗：鴻海軟體工程師，設計師轉職前端工程師，擅長將複雜觀念用圖形化方式呈現",
+      "六屆 IT 鐵人邦獲選紀錄（含五屆優選）",
+      "線上課程超過萬人註冊，技術 Blog - 卡斯伯前端",
+      "2017 Modern Web 研討會技術講者",
+      "2021 {Laravel x Vue} Conf 技術講者",
+      "經驗豐富的技術開發者，擅長跨領域技術整合",
+    ],
+  },
+];
+
+const coachImages = [ coachWeijie,coachYinmin,coachJustin,coachCasper];
 
 const TrainingProgramsSection = () => {
+  const [currentCoachIdex, setCurrentIndex] = useState(0);
   return (
     <>
       <section className="flex flex-col items-center bg-Neutral-100">
-        <h2 className="heading-2! text-Neutral-700 text-center -mt-5 mb-2 md:text-4xl! md:mb-3">
+        <h2 className="heading-2! text-Neutral-700 text-center -mt-5 mb-2 lg:text-4xl! lg:mb-3">
           關於培訓
         </h2>
         <div className="flex justify-center items-center gap-3 bg-Neutral-white px-5 py-3 w-83 rounded-[50px] shadow-[0_2px_4px_0_#E8E8E8] overflow-hidden text-Neutral-700 text-[14px] leading-normal tracking-[0.02em] mb-20 lg:w-159 lg:text-[16px]">
@@ -287,6 +355,7 @@ const TrainingProgramsSection = () => {
             </div>
           </section>
         </div>
+
         <section className="w-full pt-10 overflow-hidden xl:max-w-269 xl:mx-auto">
           <div className="heading-3! text-Neutral-700 font-bold flex w-full px-12.25 xl:max-w-296">
             <p>教練團</p>
@@ -296,46 +365,23 @@ const TrainingProgramsSection = () => {
             <div className="overflow-x-auto no-scrollbar scroll-smooth snap-x snap-mandatory scroll-pl-12.25 xl:overflow-visible xl:scroll-pl-0">
               <div className="flex gap-7 px-12.25 w-max py-6 xl:grid xl:grid-cols-4 xl:w-full xl:mx-auto xl:px-0 xl:gap-6">
                 {/* 圖片區 */}
-                <div className="relative w-83 h-50 r-lg border-2 border-Neutral-700 bg-Neutral-white shrink-0 active:bg-Primary-Blue-100 snap-start lg:w-62.75 lg:h-40">
-                  <div className="absolute -top-6 left-61.5 r-sm border-2 border-Neutral-700 text-Neutral-700 w-12.5 h-28.25 py-3 px-5 bg-Neutral-white z-2 shadow-[4px_4px_0px_0px_var(--color-Neutral-700)] whitespace-nowrap body-2! font-bold [writing-mode:vertical-rl] flex justify-center items-center lg:w-12 lg:h-26 lg:pd-4 lg:font-en lg:left-45.75">
-                    <p>廖 洧 杰</p>
+
+                {coachInfo.map((coach, index) => (
+                  <div
+                    key={coach.id}
+                    onClick={() => setCurrentIndex(index)}
+                    className={`relative w-83 h-50 r-lg border-2 border-Neutral-700  shrink-0 cursor-pointer  snap-start lg:w-62.75 lg:h-40 ${
+                      currentCoachIdex === index
+                        ? "bg-Primary-Blue-100"
+                        : "bg-Neutral-white"
+                    }`}
+                  >
+                    <div className="absolute -top-6 left-61.5 r-sm border-2 border-Neutral-700 text-Neutral-700 w-12.5 h-28.25 py-3 px-5 bg-Neutral-white z-2 shadow-[4px_4px_0px_0px_var(--color-Neutral-700)] whitespace-nowrap body-2! font-bold [writing-mode:vertical-rl] flex justify-center items-center lg:w-12 lg:h-26 lg:pd-4 lg:font-en lg:left-45.75">
+                      <p>{coach.name.split("").join(" ")}</p>
+                    </div>
+                    <img className="absolute -top-6 w-55 h-auto object-bottom-left z-2 lg:w-45" src={coachImages[index]} alt={coach.name}/>
                   </div>
-                  <img
-                    className="absolute -top-6 w-55 h-auto object-bottom-left z-2 lg:w-45"
-                    src={coachWeijie}
-                    alt="廖洧杰"
-                  />
-                </div>
-                <div className="relative w-83 h-50 r-lg border-2 border-Neutral-700 bg-Neutral-white shrink-0 active:bg-Primary-Blue-100 snap-start lg:w-62.75 lg:h-40">
-                  <div className="absolute -top-6 left-61.5 r-sm border-2 border-Neutral-700 text-Neutral-700 w-12.5 h-28.25 py-3 px-5 bg-Neutral-white z-2 shadow-[4px_4px_0px_0px_var(--color-Neutral-700)] whitespace-nowrap body-2! font-bold [writing-mode:vertical-rl] flex justify-center items-center lg:w-12 lg:h-26 lg:pd-4 lg:font-en lg:left-45.75">
-                    <p>穎 旻</p>
-                  </div>
-                  <img
-                    className="absolute -top-6 w-55 h-auto object-bottom-left lg:w-45 "
-                    src={coachYinmin}
-                    alt="穎 旻"
-                  />
-                </div>
-                <div className="relative w-83 h-50 r-lg border-2 border-Neutral-700 bg-Neutral-white shrink-0 active:bg-Primary-Blue-100 snap-start lg:w-62.75 lg:h-40">
-                  <div className="absolute -top-6 left-61.5 r-sm border-2 border-Neutral-700 text-Neutral-700 w-12.5 h-28.25 py-3 px-5 bg-Neutral-white z-2 shadow-[4px_4px_0px_0px_var(--color-Neutral-700)] whitespace-nowrap body-2! font-bold [writing-mode:vertical-lr] flex justify-center items-center lg:w-12 lg:h-26 lg:pd-4 lg:font-en lg:left-45.75">
-                    <p>賈 斯 汀</p>
-                  </div>
-                  <img
-                    className="absolute -top-6 w-55 h-auto object-bottom-left lg:w-45"
-                    src={coachJustin}
-                    alt="賈斯汀"
-                  />
-                </div>
-                <div className="relative w-83 h-50 r-lg border-2 border-Neutral-700 bg-Neutral-white shrink-0 active:bg-Primary-Blue-100 snap-start lg:w-62.75 lg:h-40">
-                  <div className="absolute -top-6 left-61.5 r-sm border-2 border-Neutral-700 text-Neutral-700 w-12.5 h-28.25 py-3 px-5 bg-Neutral-white z-2 shadow-[4px_4px_0px_0px_var(--color-Neutral-700)] whitespace-nowrap body-2! font-bold [writing-mode:vertical-rl] flex justify-center items-center lg:w-12 lg:h-26 lg:pd-4 lg:font-en lg:left-45.75">
-                    <p>卡 斯 伯</p>
-                  </div>
-                  <img
-                    className="absolute -top-6 w-55 h-auto object-bottom-left lg:w-45"
-                    src={coachCasper}
-                    alt="卡斯伯"
-                  />
-                </div>
+                ))}
               </div>
             </div>
           </div>
