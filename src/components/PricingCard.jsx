cd9da5b79d136f93ac39cda73fe333e785860563
Lin <tzu.hsun.lin046@gmail.com>
2025-12-23 21:52:59 +0800

    refactor: extract PricingCard component and use in PricingPlansSection

---
+++ src/components/PricingCard.jsx
@@ -0,0 +1,34 @@
+import check from "../assets/imgs/icon/ic-check-lg.svg";
+const PricingCard = ({ title, price, features }) => {
+  return (
+    <div
+      className={`
+        max-w-83.25 p-8 flex flex-col gap-4 items-center r-md
+        bg-Neutral-white text-Neutral-700 transition-all duration-300
+      `}
+    >
+      <h4 className="heading-3 whitespace-nowrap">{title}</h4>
+
+      <div className="w-full py-5 px-10 bg-Neutral-200 flex justify-center">
+        <p className="text-2xl font-en whitespace-nowrap">{price}</p>
+      </div>
+
+      <div className="w-full flex flex-col gap-3">
+        {features.map((item, index) => (
+          <div key={index} className="flex gap-2">
+            <img src={check} alt="check" className="w-6 h-6" />
+            {item.highlight ? (
+              <div className="relative">
+                <p className="relative z-10">{item.text}</p>
+                <span className="absolute inset-x-0 bottom-0.5 h-2/5 bg-accent-orange z-0"></span>
+              </div>
+            ) : (
+              <p>{item.text}</p>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default PricingCard;
--- src/components/PricingPlansSection.jsx
+++ src/components/PricingPlansSection.jsx
@@ -1,102 +1,61 @@
-import check from "../assets/imgs/icon/ic-check-lg.svg";
-const PricingPlansSection = () => {
+import PricingCard from './PricingCard'
 
+const PricingPlansSection = () => {
   return (
-    <section className="w-full flex flex-col items-center justify-center bg-Primary-Violet-200 px-12 pb-12 ">
+    <section className="w-full flex flex-col items-center bg-Primary-Violet-200 px-12 pb-12 pt-27.5">
       <div className="flex flex-col gap-2 mb-5 self-start">
         <h3 className="heading-2 text-Neutral-700">進駐費用</h3>
         <p className="text-Neutral-700">
-          進駐區域共分兩區塊：<span className="font-bold">培訓區域</span> 及{" "}
-          <span className="font-bold">進駐區域</span>，目前僅接受租期 1
-          個月以上，單日暫不開放。
+          進駐區域共分兩區塊：
+          <span className="font-bold">培訓區域</span> 及
+          <span className="font-bold">進駐區域</span>
+          ，目前僅接受租期 1 個月以上，單日暫不開放。
         </p>
       </div>
       <div className="w-full flex gap-5 overflow-x-auto transform-all duration-300 lg:gap-6">
-        <div className="max-w-83.25 shrink-0 p-8 flex flex-col gap-4 items-center r-md bg-Neutral-white text-Neutral-700 active:border-2 active:border-Neutral-700">
-          <h4 className="heading-3">進駐者</h4>
-          <div className="w-full py-5 px-10 bg-Neutral-200 flex items-center justify-center">
-            <p className="heading-3 font-en whitespace-nowrap">
-              $ 3,200 / Month
-            </p>
-          </div>
-          <div className="w-full flex flex-col gap-3">
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <div className="relative">
-                <p className="relative z-10">進駐區域</p>
-                <span className="absolute -inset-y-0.1 inset-x-0 bottom-0.5 h-2/5 bg-accent-orange z-0"></span>
-              </div>
-            </div>
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <p>9:00-18:00 可使用</p>
-            </div>
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <div className="relative">
-                <p className="relative z-10">固定座位</p>
-                <span className="absolute -inset-y-0.1 inset-x-0 bottom-0.5 h-2/5 bg-accent-orange z-0"></span>
-              </div>
-            </div>
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <div className="relative">
-                <p className="relative z-10">專屬置物櫃</p>
-                <span className="absolute -inset-y-0.1 inset-x-0 bottom-0.5 h-2/5 bg-accent-orange z-0"></span>
-              </div>
-            </div>
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <p>免費無線網路</p>
-            </div>
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <p>免費咖啡茶水</p>
-            </div>
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <p>原價｜3,500 / Month</p>
-            </div>
-          </div>
-        </div>
-        <div className="smax-w-83.25 shrink-0 p-8 flex flex-col gap-4 items-center r-md bg-Neutral-white text-Neutral-700 active:border-2 active:border-Neutral-700">
-          <h4 className="heading-3 whitespace-nowrap">單日使用（暫不提供）</h4>
-          <div className="w-full py-5 px-10 bg-Neutral-200 flex items-center justify-center">
-            <p className="heading-3 font-en whitespace-nowrap">$ 300 / Day</p>
-          </div>
-          <div className="w-full flex flex-col gap-3">
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <p>培訓區域</p>
-            </div>
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <p>9:00-18:00 可使用</p>
-            </div>
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <p>非固定座位</p>
-            </div>
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <p>共用置物櫃</p>
-            </div>
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <p>免費無線網路</p>
-            </div>
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <p>免費咖啡茶水</p>
-            </div>
-            <div className="flex gap-2">
-              <img src={check} alt="check" className="w-6 h-6 aspect-square" />
-              <p>原價｜300 / Day</p>
-            </div>
-          </div>
-        </div>
+        <PricingCard
+          title="進駐者"
+          price="$ 3,200 / Month"
+          features={[
+            { text: "進駐區域", highlight: true },
+            { text: "9:00-18:00 可使用" },
+            { text: "固定座位", highlight: true },
+            { text: "專屬置物櫃", highlight: true },
+            { text: "免費無線網路" },
+            { text: "免費咖啡茶水" },
+            { text: "原價｜3,500 / Month" },
+          ]}
+        />
+
+        <PricingCard
+          title="單日使用（暫不提供）"
+          price="$ 300 / Day"
+          features={[
+            { text: "培訓區域" },
+            { text: "9:00-18:00 可使用" },
+            { text: "非固定座位" },
+            { text: "共用置物櫃" },
+            { text: "免費無線網路" },
+            { text: "免費咖啡茶水" },
+            { text: "原價｜300 / Day" },
+          ]}
+        />
+      </div>
+
+      <div className="w-full flex justify-center items-center bg-Primary-Violet-200 border-2 border-Neutral-700 r-md mt-8 py-4 lg:py-6">
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLScEUjtPalxIGn0dVgO_1VLFxuq7is_0VUYROiGzktwze5QARw/viewform"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="heading-3 text-Neutral-700"
+        >
+          馬上填表單!
+        </a>
       </div>
     </section>
   );
 };
+
+
+
 export default PricingPlansSection;
