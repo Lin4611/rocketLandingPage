abcc25fcc5f8b68b21ac31fa1bf32048a23b8055
tsfmakko <racingf1@gmail.com>
2025-12-23 15:40:30 +0800

    feat: initial

---
+++ src/components/SharedSpaceSection.jsx
@@ -0,0 +1,9 @@
+import { section } from "motion/react-client";
+
+const SharedSpaceSection = () => {
+    return (
+        <section>initial</section>
+    )
+};
+
+export default SharedSpaceSection;
