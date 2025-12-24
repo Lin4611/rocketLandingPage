830bd4ddb99a83fa21527d23726e2b70e7a01b96
Lin <tzu.hsun.lin046@gmail.com>
2025-12-24 15:56:30 +0800

    style: add notice section with RWD adjustment

--- src/components/PricingPlansSection.jsx
+++ src/components/PricingPlansSection.jsx
@@ -4,12 +4,37 @@ import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import { useState } from "react";
+import icon_go from "../assets/imgs/icon/ic-go-coworking.svg";
 const PricingPlansSection = () => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [isHover, setIsHover] = useState(false);
   return (
     <section className="w-full bg-Primary-Violet-200 flex justify-center">
-      <div className="w-full max-w-293 flex flex-col px-12 pb-12 pt-27.5">
-        <div className="flex flex-col gap-2 mb-5 self-start">
+      <div className="w-full max-w-293 flex flex-col px-12 pb-12 pt-9 relative lg:pt-15 lg:pb-20 ">
+        <div className="relative mb-7.25 max-w-116.5 w-full md:mb-6.25 md:pl-20">
+          <p className="r-sm outline-2 outline-Neutral-700  bg-white px-2.5 py-3 text-Neutral-700 text-sm md:px-4 md:text-base">
+            <span className="font-en font-bold">2023</span>
+            <span> 年為試營運階段，進駐可享有</span>
+            <span className="font-bold">限定優惠價！</span>
+          </p>
+          <span
+            className="
+                        absolute left-5.5 md:left-23 -bottom-3.75
+                        h-0 w-0
+                        border-l-13 border-l-transparent
+                        border-r-12 border-r-transparent
+                        border-t-15 border-t-Neutral-700"
+          />
+          <span
+            className="
+                        absolute left-6 md:left-23 -bottom-3
+                        h-0 w-0
+                        border-l-11 border-l-transparent
+                        border-r-11 border-r-transparent
+                        border-t-15 border-t-white"
+          />
+        </div>
+        <div className="flex flex-col gap-2 mb-5 self-start ">
           <h3 className="heading-2 text-Neutral-700">進駐費用</h3>
           <p className="text-Neutral-700">
             進駐區域共分兩區塊：
@@ -18,6 +43,7 @@ const PricingPlansSection = () => {
             ，目前僅接受租期 1 個月以上，單日暫不開放。
           </p>
         </div>
+
         <Swiper
           modules={[Pagination]}
           spaceBetween={20}
@@ -51,7 +77,7 @@ const PricingPlansSection = () => {
             <PricingCard
               title="進駐者"
               price="$ 3,200 / Month"
-              isActive={activeIndex === 0} 
+              isActive={activeIndex === 0}
               onClick={() => setActiveIndex(0)}
               features={[
                 { text: "進駐區域", highlight: true },
@@ -68,7 +94,7 @@ const PricingPlansSection = () => {
             <PricingCard
               title="單日使用（暫不提供）"
               price="$ 300 / Day"
-              isActive={activeIndex === 1} 
+              isActive={activeIndex === 1}
               onClick={() => setActiveIndex(1)}
               features={[
                 { text: "培訓區域" },
@@ -87,9 +113,25 @@ const PricingPlansSection = () => {
           href="https://docs.google.com/forms/d/e/1FAIpQLScEUjtPalxIGn0dVgO_1VLFxuq7is_0VUYROiGzktwze5QARw/viewform"
           target="_blank"
           rel="noopener noreferrer"
-          className="w-full flex justify-center items-center bg-Primary-Violet-200 border-2 border-Neutral-700 r-md mt-8 py-4 lg:py-6"
+          className="w-full flex justify-center items-center bg-Primary-Violet-200 border-2 border-Neutral-700 r-md mt-8 h-24"
+          onMouseEnter={() => {
+            setIsHover(true);
+          }}
+          onMouseLeave={() => {
+            setIsHover(false);
+          }}
+          onClick={() => {
+            setIsHover(false);
+          }}
         >
           <span className="heading-3 text-Neutral-700">馬上填表單!</span>
+          <img
+            src={icon_go}
+            alt="go"
+            className={`w-16 h-16 aspect-square ${
+              isHover ? "scale-100 ml-5" : "scale-0"
+            } transition-all duration-300 hidden lg:block`}
+          />
         </a>
       </div>
     </section>
