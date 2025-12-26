c719f0d5d967126cf31b9c7c36a1b77c1c4340dd
Lin <tzu.hsun.lin046@gmail.com>
2025-12-26 19:32:43 +0800

    fix: wrap HomePage content with main element

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,13 +5,13 @@ import EnrollmentInfoSection from "../components/EnrollmentInfoSection";
 import PartnersSection from "../components/PartnersSection";
 const HomePage = () => {
     return(
-        <>
+        <main className="w-full mx-auto">
         <AboutRocketSection/>
         <SuccessCasesSection/>
         <TrainingProgramsSection/>
         <EnrollmentInfoSection/>
         <PartnersSection/>
-        </>
+        </main>
     )
 };
 export default HomePage;
