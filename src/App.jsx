0aa7778b070e989f75735e622f90577f5a0ca3b0
tsfmakko <racingf1@gmail.com>
2025-12-26 13:54:57 +0800

    chore: sync with dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Header from "./components/Header";
+import WebRouter from "./router/WebRouter";
+import Footer from "./components/Footer";
+import Marquee from "./components/Marquee";
 function App() {
   return (
     <>
+    <Header />
+    <WebRouter />
+    <Footer />
+    <Marquee />
     </>
   );
 }
